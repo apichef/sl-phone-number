6965296d11fdc35e7b341a3b32c31164ba952caa
Nimna Awantha <avantha.nimna@gmail.com>
2024-08-18 16:41:19 +0530

    support php 8.2 higher

--- tests/PhoneNumberTest.php
+++ tests/PhoneNumberTest.php
@@ -5,12 +5,11 @@ declare(strict_types=1);
 namespace APiChef\SlPhoneNumber;
 
 use PHPUnit\Framework\TestCase;
+use PHPUnit\Framework\Attributes\DataProvider;
 
 class PhoneNumberTest extends TestCase
 {
-    /**
-     * @dataProvider phoneNumbersWithValidity
-     */
+    #[DataProvider('phoneNumbersWithValidity')]
     public function test_getDetails_throws_an_exception_when_the_starting_digits_or_length_is_invalid(string $number, bool $validity, string $formated = null)
     {
         $slPhoneNumber = new PhoneNumber($number);
@@ -25,9 +24,7 @@ class PhoneNumberTest extends TestCase
         $this->assertEquals($formated, $result->getNumber());
     }
 
-    /**
-     * @dataProvider phoneNumbersWithValidity
-     */
+    #[DataProvider('phoneNumbersWithValidity')]
     public function test_it_validates_phone_number_by_starting_digits_and_length(string $number, bool $validity)
     {
         $slPhoneNumber = new PhoneNumber($number);
@@ -35,26 +32,26 @@ class PhoneNumberTest extends TestCase
         $this->assertEquals($validity, $slPhoneNumber->isValid());
     }
 
-    public function phoneNumbersWithValidity()
+    public static function phoneNumbersWithValidity()
     {
         return [
-            ['0770000000', true, '0770000000'], // 10 digits
-            ['0112000000', true, '0112000000'], // 10 digits
-            ['1000000000', false, null], // 10 digits NOT starting with 0
-            ['000000000', false, null], // 9 digits
-            ['00000000000', false, null], // 11 digits
+            ['0770000000', true, '0770000000'],
+            ['0112000000', true, '0112000000'],
+            ['1000000000', false, null],
+            ['000000000', false, null],
+            ['00000000000', false, null],
 
-            ['+94770000000', true, '0770000000'], // +94 and 9 digits
-            ['+94112000000', true, '0112000000'], // +94 and 9 digits
-            ['-94000000000', false, null], // -94 and 9 digits
-            ['+9400000000', false, null], // +94 and 8 digits
-            ['+940000000000', false, null], // +94 and 10 digits
+            ['+94770000000', true, '0770000000'],
+            ['+94112000000', true, '0112000000'],
+            ['-94000000000', false, null],
+            ['+9400000000', false, null],
+            ['+940000000000', false, null],
 
-            ['0094770000000', true, '0770000000'], // 0094 and 9 digits
-            ['0094112000000', true, '0112000000'], // 0094 and 9 digits
-            ['0096000000000', false, null], // 0096 and 9 digits
-            ['009400000000', false, null], // 0094 and 8 digits
-            ['00940000000000', false, null], // 0094 and 10 digits
+            ['0094770000000', true, '0770000000'],
+            ['0094112000000', true, '0112000000'],
+            ['0096000000000', false, null],
+            ['009400000000', false, null],
+            ['00940000000000', false, null],
         ];
     }
 
@@ -95,25 +92,21 @@ class PhoneNumberTest extends TestCase
         $details->getArea();
     }
 
-    /**
-     * @dataProvider validityPhoneNumbers
-     */
+    #[DataProvider('validityPhoneNumbers')]
     public function test_can_get_local_format(string $number)
     {
         $localFormat = (new PhoneNumber($number))->toLocalFormat();
         $this->assertEquals('0710000000', $localFormat);
     }
 
-    /**
-     * @dataProvider validityPhoneNumbers
-     */
+    #[DataProvider('validityPhoneNumbers')]
     public function test_can_get_IDD_format(string $number)
     {
         $localFormat = (new PhoneNumber($number))->toIDDFormat();
         $this->assertEquals('+94710000000', $localFormat);
     }
 
-    public function validityPhoneNumbers(): array
+    public static function validityPhoneNumbers(): array
     {
         return [
             ['0710000000'],
